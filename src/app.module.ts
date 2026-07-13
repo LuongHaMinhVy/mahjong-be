33bf8b11e010583ef2962d7bde3595fc85b50378
CanhVeTaiGia <lvy16012005@gmail.com>
2026-07-13 09:26:07 +0700

    feat: import PrismaModule and RedisModule into AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './shared/database/prisma.module';
+import { RedisModule } from './shared/redis/redis.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    RedisModule,
+  ],
 })
 export class AppModule {}
