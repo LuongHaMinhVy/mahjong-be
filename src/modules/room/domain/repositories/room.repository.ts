122a17671d7d75887358d49469efa9a473eb6f86
CanhVeTaiGia <lvy16012005@gmail.com>
2026-07-13 16:33:16 +0700

    feat(room): define IRoomRepository port interface

---
+++ src/modules/room/domain/repositories/room.repository.ts
@@ -0,0 +1,8 @@
+import { Room } from '../entities/room.entity.js';
+
+export abstract class IRoomRepository {
+  abstract save(room: Room): Promise<void>;
+  abstract findById(id: string): Promise<Room | null>;
+  abstract delete(id: string): Promise<void>;
+  abstract findAllWaiting(): Promise<Room[]>;
+}
